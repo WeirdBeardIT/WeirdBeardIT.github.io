904f4705a9250fe9891b0a0b1fcefeff7c84b7be
WeirdBeardIT <weirdbeardit@hotmail.com>
2024-11-25 23:53:00 +0000

    Update darkmode.js

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,30 +1,34 @@
 var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
 var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 
-// Set dark mode as the default theme
+// Function to update theme icons
+function updateThemeIcons(isDark) {
+  if (isDark) {
+    themeToggleLightIcon.classList.remove('hidden');
+    themeToggleDarkIcon.classList.add('hidden');
+  } else {
+    themeToggleLightIcon.classList.add('hidden');
+    themeToggleDarkIcon.classList.remove('hidden');
+  }
+}
+
+// Set dark mode as the default theme if not set
 if (localStorage.getItem('color-theme') === 'dark' || 
     (!localStorage.getItem('color-theme') && 
     !window.matchMedia('(prefers-color-scheme: light)').matches)) {
     document.documentElement.classList.add('dark');
-    themeToggleLightIcon.classList.remove('hidden');
+    localStorage.setItem('color-theme', 'dark');
+    updateThemeIcons(true);
 } else {
     document.documentElement.classList.remove('dark');
-    themeToggleDarkIcon.classList.remove('hidden');
+    updateThemeIcons(false);
 }
 
 var themeToggleBtn = document.getElementById('theme-toggle');
 
 themeToggleBtn.addEventListener('click', function() {
-    // Toggle icons inside button
-    themeToggleDarkIcon.classList.toggle('hidden');
-    themeToggleLightIcon.classList.toggle('hidden');
-
     // Toggle dark and light mode
-    if (document.documentElement.classList.contains('dark')) {
-        document.documentElement.classList.remove('dark');
-        localStorage.setItem('color-theme', 'light');
-    } else {
-        document.documentElement.classList.add('dark');
-        localStorage.setItem('color-theme', 'dark');
-    }
+    const isDark = document.documentElement.classList.toggle('dark');
+    localStorage.setItem('color-theme', isDark ? 'dark' : 'light');
+    updateThemeIcons(isDark);
 });
